1c87dc3ed1cec3521185eb613a33c6efe4858a1f
VladkoMAC <v.cimbora123@gmail.com>
2019-04-15 10:35:00 +0200

    Init commit

---
+++ lib/data.dart
@@ -0,0 +1,13 @@
+List<String> images = [
+  'assets/image_01.png',
+  'assets/image_02.jpg',
+  'assets/image_03.jpg',
+  'assets/image_04.jpg',
+];
+
+List<String> title = [
+  'Hounted Ground',
+  "Fallen in Love",
+  "The dreaming moon",
+  "Jack the Persian and the Black Castel"
+];
